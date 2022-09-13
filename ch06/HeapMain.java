f4475815788c029303082a911db521ff8c3508e6
seoyoung78 <tjdud0708@naver.com>
2022-09-13 19:17:57 +0900

    힙 트리

---
+++ ch06/HeapMain.java
@@ -0,0 +1,22 @@
+package ch06;
+
+public class HeapMain {
+    /**
+     * 최소 힙 트리 삽입 예제
+     * @param args
+     */
+    public static void main(String[] args) {
+        MinHeapTree minHeap = new MinHeapTree(6);
+        minHeap.insert(14);
+        minHeap.insert(11);
+        minHeap.insert(6);
+        minHeap.insert(12);
+        minHeap.insert(5);
+        minHeap.insert(3);
+
+        minHeap.print();
+
+        minHeap.delete();
+        minHeap.print();
+    }
+}
