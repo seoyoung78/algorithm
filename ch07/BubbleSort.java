62ed5f2865f16179fdb3db189cf13607e9c819c4
seoyoung78 <tjdud0708@naver.com>
2022-09-19 20:02:52 +0900

    버블 정렬

---
+++ ch07/BubbleSort.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class BubbleSort {
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
---
+++ ch07/Main.java
@@ -0,0 +1,14 @@
+package ch07;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int[] arr = new int[]{8, 2, 14, 7, 5};
+        bubbleSort.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
